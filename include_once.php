f800d01ca139cd95a5b9881b55d1558691585985
Rania Ayuni <raniaaayunii@gmail.com>
2022-12-29 11:21:23 +0700

    web modul 9

---
+++ include_once.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Penggunaan Modularitasi</title>
+    </head>
+    <body>
+        <h2>Penggunaan include once</h2>
+        <pre>
+            <?php
+            include_once("script.php");
+            include_once("script.php");
+            ?>
+        </pre>
+    </body>
+</html>
